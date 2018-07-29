f5d8dd45c23b3f72f86f88cdff845279e491b21f
Stefan Hiebl <stefan.hiebl@gmx.com>
2018-07-29 17:59:24 -0500

    Added appearance of table view; needs fix though

--- FLAGSCAN/ViewController.swift
+++ FLAGSCAN/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+/*
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -22,4 +22,33 @@ class ViewController: UIViewController {
 
 
 }
+*/
+
+//  Actual code from Stefan Hiebl
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    // Number of sections in the table
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    // How many rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    // Content of each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Test Cell"
+        return cell
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+
+
 
