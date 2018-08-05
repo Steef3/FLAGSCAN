223fe1ae9753c87f614d49f5fbc26329eedbd424
Stefan Hiebl <stefan.hiebl@gmx.com>
2018-08-05 14:14:20 -0500

    Added VC for Settings

---
+++ FLAGSCAN/SettingsVC.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsVC.swift
+//  FLAGSCAN
+//
+//  Created by Stefan Hiebl on 8/5/18.
+//  Copyright © 2018 Stefan Hiebl. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
